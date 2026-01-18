5ef11cf61092bbb3a92ea4d85dc55966434b3696
hoangda24312 <thanosligma942@gmail.com>
2026-01-18 15:27:49 +0700

    +xong set status order, con thieu cap nhat ben database

--- RestaurantOrderManagement/RestaurantOrderManagement/Order.cpp
+++ RestaurantOrderManagement/RestaurantOrderManagement/Order.cpp
@@ -1,6 +1,8 @@
 #include"Order.h"
 #include"Database.h"
 
+Database db;
+
 Order::Order(const int order_id,const int table_number,const std::chrono::system_clock::time_point& order_time,const OrderStatus status,
 	const float total_amount,const std::string& note,const std::string& customer_name):
 	order_id(order_id), table_number(table_number), order_time(order_time), status(status),
@@ -24,8 +26,29 @@ OrderStatus Order::getStatus() const
 {
 	return this->status;
 }
-void cancel();
-void sendToKitchen();
-void markPreparing();
-void markReady();
-void markCompleted();
+
+
+void Order::cancel()
+{
+	if (this->status == OrderStatus::COMPLETED || this->status == OrderStatus::PREPARING || this->status == OrderStatus::READY)
+	{
+		throw std::logic_error("Order already cooked, can't cancel");
+	}
+	this->status = OrderStatus::CANCELLED;
+}
+void Order::sendToKitchen()
+{
+	this->status = OrderStatus::PENDING;
+}
+void Order::markPreparing()
+{
+	this->status = OrderStatus::PREPARING;
+}
+void Order::markReady()
+{
+	this->status = OrderStatus::READY;
+}
+void Order::markCompleted()
+{
+	this->status = OrderStatus::COMPLETED;
+}
