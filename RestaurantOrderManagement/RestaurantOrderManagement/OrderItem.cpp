2a1514453e7eade1172e0990a66113a4091dad69
hoangda24312 <thanosligma942@gmail.com>
2026-01-21 16:16:14 +0700

    + xong order_item

--- RestaurantOrderManagement/RestaurantOrderManagement/OrderItem.cpp
+++ RestaurantOrderManagement/RestaurantOrderManagement/OrderItem.cpp
@@ -5,6 +5,12 @@ OrderItem::OrderItem(const std::string& order_item_id, const int quantity, const
 {
 }
 
+//use to create an orderitem with quantity and price from menu_item
+OrderItem OrderItem::create(const std::string& order_item_id,const MenuItem& menu_item, int quantity)
+{
+	OrderItem order_item(order_item_id, quantity, menu_item.getPrice());
+	return order_item;
+}
 
 float OrderItem::calculateCost() const
 {
--- RestaurantOrderManagement/RestaurantOrderManagement/OrderItem.h
+++ RestaurantOrderManagement/RestaurantOrderManagement/OrderItem.h
@@ -9,6 +9,7 @@ private:
 	float price;
 public:
 	OrderItem(const std::string& order_item_id,const int quantity,const float price);
+	static OrderItem create(const std::string& order_item_id,const MenuItem& menu_item, int quantity);
 	int getItemNo() const;
 	float calculateCost() const;
 };
